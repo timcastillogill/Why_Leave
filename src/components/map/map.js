01b9c50a90fa85aa5bda8c02f7004968d7430aa4
jotkafomat <k-j@xl.wp.pl>
2020-12-11 16:38:50 +0000

    Add markers

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps"
+import {GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from "react-google-maps"
 
+import * as parkData from "./data/skateboard-parks.json";
 function GoogleMaps() {
   return (
      <GoogleMap
       defaultZoom={2.5}
       defaultCenter={{lat: 51.39305, lng: -0.304320}}
-      />
+      >
+      {parkData.features.map((park)=> (
+        <Marker key={park.properties.PARK_ID} position={{
+          lat: park.geometry.coordinates[1],
+          lng: park.geometry.coordinates[0]
+        }}
+        />
+      ))}
+      </GoogleMap>
   );
 }
 
@@ -16,7 +29,7 @@ export default function Map() {
   return (
   < div style={{width: '100vw', height: '100vh'}}>
   <WrappedMap
-    googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key='}
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
     loadingElement={<div style={{ height: "100%" }}/>}
     containerElement={<div style={{ height: "100%" }}/>}
     mapElement={<div style={{ height: "100%" }}/>}
