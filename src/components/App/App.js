bca5663e513d223ba08d227263853cb27b3ad4d6
Beca-g <rebekahgalliano@gmail.com>
2020-12-13 22:04:26 +0000

    Fixing bugs
    - Updated React.StrictMode in index.js to React.Fragment
    - Updated App.js - removed unused imports
    - Updated package.json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
 import '../App/App.css'
-import Business from '../country/country.js';
-import News from '../news/news.js';
 import Map from '../map/map.js';
+
 function App() {
   return (
     <div className="App">
-    <Map />
-      // < Business />
-      // <News />
+      <Map />
     </div>
   );
 }
