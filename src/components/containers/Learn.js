251f66dc00ec6cad9279ace2da95d8a4a0b9631f
Tim Castillo-Gill <timgillviola@gmail.com>
2020-12-17 15:25:11 +0000

    Made folders and started heading

---
+++ src/components/containers/Learn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Learn.css";
+
+export default function Learn() {
+  return (
+    <div className="Learn">
+      <div className="lander">
+        <h1>Why Do People Leave Their Countries?</h1>
+        <p>There are many reasons why it might be too difficult or dangerous for people to stay in their own countries. For example, children, woman and men flee from violence, war, hunger, extreme poverty, because of their sexual or gender orientation, or from the consequences of climate change or other natural disasters. Often people will face a combination of these difficult circumstances. People who leave their countries are not always fleeing danger. They might believe they have a better chance of finding work in another country because they have the education or capital to seek opportunities elsewhere. Others might want to join relatives or friends who are already living abroad. Or they might seek to start or finish their education in another country. There are lots of different reasons for people to start a journey to build a life in a new country.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import About from "../containers/About";
 import Support from "../containers/Support";
+import Learn from "../containers/Learn";
 
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/learn">
+        <Learn />
+      </Route>
       <Route exact path="/about">
         <About />
       </Route>
