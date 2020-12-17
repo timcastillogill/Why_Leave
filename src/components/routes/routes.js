f25f121a92b0d84d5ed5b5cae9c2565f062d5d4e
Otis <otisvickersgraver@gmail.com>
2020-12-17 22:41:59 +0000

    fix conflicts

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import About from "../containers/About";
 import Support from "../containers/Support";
 
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/about">
-        <About />
-      </Route>
       <Route exact path="/support">
         <Support />
       </Route>
