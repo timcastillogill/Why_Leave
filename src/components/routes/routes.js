b9571cdbdfb866b8c08b65d44aa736069fd52465
Beca-g <rebekahgalliano@gmail.com>
2020-12-17 23:21:50 +0000

    Fixing support css issues

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import About from "../containers/About";
 import Support from "../containers/Support";
 import AboutWhyLeave from "../containers/AboutWhyLeave";
 import Learn from "../containers/Learn";
@@ -10,9 +9,6 @@ export default function Routes() {
     <Switch>
       <Route exact path="/learn">
         <Learn />
-      </Route>
-      <Route exact path="/about">
-        <About />
       </Route>
       <Route exact path="/support">
         <Support />
