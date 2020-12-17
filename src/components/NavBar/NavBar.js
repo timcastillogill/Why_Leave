df5488107ab6a28abc24c3f831ef7226c6bc5619
Beca-g <rebekahgalliano@gmail.com>
2020-12-17 14:07:01 +0000

    Adjusted Navbar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,9 +5,9 @@ import * as ReactBootStrap from 'react-bootstrap';
 export default function NavBar() {
   return (
     <div className="nav-bar">
-       <ReactBootStrap.Navbar bg="light" variant="light">
+       <ReactBootStrap.Navbar >
         <ReactBootStrap.Navbar.Brand href="/">Why Leave?</ReactBootStrap.Navbar.Brand>
-          <ReactBootStrap.Nav className="mr-auto">
+          <ReactBootStrap.Nav className="ml-auto">
               <ReactBootStrap.Nav.Link float="left" href="/">Home</ReactBootStrap.Nav.Link>
               <ReactBootStrap.Nav.Link href="/about">About</ReactBootStrap.Nav.Link>
               <ReactBootStrap.Nav.Link href="/support">Support</ReactBootStrap.Nav.Link>
