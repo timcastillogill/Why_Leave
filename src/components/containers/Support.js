c622417a6534ebfa0e06a32e65bc5b432dd801e3
Beca-g <rebekahgalliano@gmail.com>
2020-12-17 22:38:37 +0000

    Updated support page including css

--- src/components/containers/Support.js
+++ src/components/containers/Support.js
@@ -3,20 +3,23 @@ import "./Support.css";
 
 export default function Support() {
   return (
-    <div className="Home">
-      <div className="lander">
-        <h1>Ways to support:</h1>
-        <p className="text-muted">Follow the links for more information</p>
-        <div classname="web">
-        <p> Maggie's Hibberd project that has helped many: <a href="https://www.welcomepresents.com/" rel="noreferrer"> Welcome Presents </a></p>
-        <p>  <a href="https://www.amnesty.org/en/" target="_blank" rel="noreferrer"> Amnesty International </a></p>
-        <p>  <a href="https://www.unhcr.org/" target="_blank" rel="noreferrer"> The UN Refugee Agency </a></p>
-        <p>  <a href="https://www.hrw.org/"target="_blank" rel="noreferrer"> Human Rights Watch </a></p>
-        <p>  <a href="https://www.refugee-action.org.uk/" target="_blank" rel="noreferrer"> Refugee Action </a></p>
-        <p>  <a href="https://www.refugeecouncil.org.uk/" target="_blank" rel="noreferrer"> Refugee Council </a></p>
-        <p>  <a href="https://www.msf.org/" target="_blank" rel="noreferrer"> Médecins Sans Frontières </a></p>
-        <p>  <a href="https://www.hias.org/" target="_blank" rel="noreferrer"> HIAS </a></p>
+    <div className="box-of-boxes">
+      <div className="circ">
+        <div className="support-text container">
+          <h1>Ways to support</h1>
+          <br></br>
+          <h5 className="text">Follow the links for more information</h5>
+          <div classname="web">
+            <h5>  <a href="https://www.welcomepresents.com/" rel="noreferrer"> Welcome Presents </a></h5>
+            <h5>  <a href="https://www.amnesty.org/en/" target="_blank" rel="noreferrer"> Amnesty International </a></h5>
+            <h5>  <a href="https://www.unhcr.org/" target="_blank" rel="noreferrer"> The UN Refugee Agency </a></h5>
+            <h5>  <a href="https://www.hrw.org/"target="_blank" rel="noreferrer"> Human Rights Watch </a></h5>
+            <h5>  <a href="https://www.refugee-action.org.uk/" target="_blank" rel="noreferrer"> Refugee Action </a></h5>
+            <h5>  <a href="https://www.refugeecouncil.org.uk/" target="_blank" rel="noreferrer"> Refugee Council </a></h5>
+            <h5>  <a href="https://www.msf.org/" target="_blank" rel="noreferrer"> Médecins Sans Frontières </a></h5>
+            <h5>  <a href="https://www.hias.org/" target="_blank" rel="noreferrer"> HIAS </a></h5>
           </div>
+        </div>
       </div>
     </div>
   );
