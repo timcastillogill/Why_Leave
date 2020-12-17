11ca1aa95e2d062930b5758d4f186a2c6a10f667
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-12-17 17:49:23 +0000

    Added links to support

--- src/components/containers/Support.js
+++ src/components/containers/Support.js
@@ -5,8 +5,18 @@ export default function Support() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>Ways to support:</h1>
+        <p className="text-muted">Follow the links for more information</p>
+        <div classname="web">
+        <p> Maggie's Hibberd project that has helped many: <a href="https://www.welcomepresents.com/"> Welcome Presents </a></p>
+        <p>  <a href="https://www.amnesty.org/en/" target="_blank" > Amnesty International </a></p>
+        <p>  <a href="https://www.unhcr.org/" target="_blank"> The UN Refugee Agency </a></p>
+        <p>  <a href="https://www.hrw.org/"target="_blank"> Human Rights Watch </a></p>
+        <p>  <a href="https://www.refugee-action.org.uk/" target="_blank"> Refugee Action </a></p>
+        <p>  <a href="https://www.refugeecouncil.org.uk/" target="_blank"> Refugee Council </a></p>
+        <p>  <a href="https://www.msf.org/" target="_blank"> Médecins Sans Frontières </a></p>
+        <p>  <a href="https://www.hias.org/" target="_blank"> HIAS </a></p>
+          </div>
       </div>
     </div>
   );
