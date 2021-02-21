134744de621ac638e07435652e0a00e86ad58c77
Otis <otisvickersgraver@gmail.com>
2021-02-21 17:22:18 +0000

    Moved Router to routes

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Support from "../containers/Support";
 import AboutWhyLeave from "../containers/AboutWhyLeave";
 import Learn from "../containers/Learn";
-import Map from "../map/map";
+import Map from "../map/Map";
 
 export default function Routes() {
   return (
+  <Router>
     <Switch>
       <Route exact path="/">
         <Map />
@@ -21,5 +22,6 @@ export default function Routes() {
         <AboutWhyLeave />
       </Route>
     </Switch>
+  </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
