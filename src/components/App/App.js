a2739f62d0dc556d809836b6304076d5c74580b6
Otis <otisvickersgraver@gmail.com>
2021-02-21 16:50:21 +0000

    Remove Map component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import '../App/App.css'
-import Map from '../map/map.js';
 import NavBar from '../NavBar/NavBar';
 import Routes from '../routes/routes.js';
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes />
-      <Map />
     </div>
   );
 }
