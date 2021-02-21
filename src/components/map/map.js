539b0dd690bd648afc443a93b023cc0b407e2590
Otis <otisvickersgraver@gmail.com>
2021-02-21 18:23:51 +0000

    Remove commented code

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -29,15 +29,9 @@ function GoogleMaps() {
 
   return (
     <GoogleMap
-    // options={{
-    //   // draggable: false,
-    //   // scrollwheel: false,
-    //   // disableDoubleClickZoom: true,
-    // }}
       defaultZoom={3}
       defaultCenter={{ lat: 28, lng: 1.6 }}
       defaultOptions={{ styles: mapStyles, minZoom: 3, maxZoom: 3, disableDefaultUI: true, gestureHandling: "greedy" }}
-
     >
       {data?.map((country) => (
         <Marker
